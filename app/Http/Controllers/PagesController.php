d7caf76beddbd0c56d2d7aea311ff7e7c4b1cc2b
Anthony Perez <anthonyacostaperez@gmail.com>
2020-05-02 21:19:22 -0700

    Updated PageController class to PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class PagesController extends Controller
 {
     public function home() {
       return view('pages.home');
