da7741c63e97bbd0c33617829f0145c4771554d7
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-06-08 11:59:27 -0400

    Remove node_modules from VCS

--- node_modules/listr2/dist/utils/renderer.d.ts
+++
@@ -1,3 +0,0 @@
-import { SupportedRenderer } from './renderer.interface';
-import { ListrRendererValue } from '../interfaces/listr.interface';
-export declare function getRenderer(renderer: ListrRendererValue, fallbackRenderer?: ListrRendererValue): SupportedRenderer;
