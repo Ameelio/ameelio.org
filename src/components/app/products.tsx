415337e399da1d6f8258f2d1eb0dc0b654ce07a8
Anthony Perez <anthonyacostaperez@gmail.com>
2020-06-12 08:34:42 -0700

    Completed products card

--- src/components/app/products.tsx
+++ src/components/app/products.tsx
@@ -3,8 +3,46 @@ import "./stylesheets/products.css";
 
 const Products: React.FC = () => {
   return (
-    <div className="products-container">
+    <div className="products-wrapper">
       <div className="products-title">Our Products</div>
+      <div className="products-container">
+        <div className="products-card-wrapper">
+          <div className="products-card-title">Letters for Families</div>
+          <div className="products-card-body">
+            Send free letters &amp; photos to your incarcerated loved ones. We
+            service all correctional and immigrant detention facilities in the
+            United States.
+          </div>
+          <button className="products-signup-button">Sign Up</button>
+          <a className="learn-more" href="#">
+            Learn More
+          </a>
+        </div>
+        <div className="products-card-wrapper">
+          <div className="products-card-title">Letters for Organizations</div>
+          <div className="products-card-body">
+            Your electronic courier is here. We'll handle your bulk mail,
+            organize your contacts, and ease collaboration and visibility within
+            your organization.
+          </div>
+          <button className="products-signup-button">Sign Up</button>
+          <a className="learn-more" href="#">
+            Learn More
+          </a>
+        </div>
+        <div className="products-card-wrapper">
+          <div className="products-card-title">Connect Videoconferencing</div>
+          <div className="products-card-body">
+            The next-generation platform for prison videoconferencing. Secure
+            and reliable for correctional facilities. Free and easy for the
+            incarcerated and their loved ones.
+          </div>
+          <button className="products-signup-button">Sign Up</button>
+          <a className="learn-more" href="#">
+            Learn More
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
